2c5f48863d459fd5416ef791dbc104a3c6b54a81
user_name <email_id>
2019-12-16 13:55:56 +0700

    add route playlist and auth for SongController

--- app/Http/Controllers/SongController.php
+++ app/Http/Controllers/SongController.php
@@ -15,6 +15,7 @@ class SongController extends Controller
     public function __construct(SongService $songService)
     {
         $this->songService = $songService;
+        $this->middleware('auth');
     }
 
     public function create()
